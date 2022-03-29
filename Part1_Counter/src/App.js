0e52d0394adbd426345349a8e0ce8e6d0521bd57
Abdelilah301 <38250371+Abdelilah301@users.noreply.github.com>
2022-03-29 15:45:04 +0000

    Create App.js

---
+++ Part1_Counter/src/App.js
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => { setCounter(counter + 1) }
+  const decreaseByOne = () => { setCounter(counter - 1) }
+  const setToZero = () => { setCounter(0) }
+  return (
+    <div>
+      <Display counter={counter} />
+
+      <Button
+        onClick={increaseByOne}
+        text="Plus"
+      />
+
+      <Button
+        onClick={decreaseByOne}
+        text="Minus"
+      />
+
+      <Button
+        onClick={setToZero}
+        text="Reset"
+      />
+
+    </div>
+
+  )
+}
+
+export default App
