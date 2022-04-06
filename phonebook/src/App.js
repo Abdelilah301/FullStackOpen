5e8c4369e35d46e179a4b326d29af46856b5abd2
Abdelilah301 <38250371+Abdelilah301@users.noreply.github.com>
2022-04-06 18:37:34 +0000

    Add files via upload

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import personService from './services/persons';
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -11,10 +12,10 @@ const App = () => {
   const [newFilter, setFilter] = useState('');
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPerson => {
+        setPersons(initialPerson);
       })
   }, [])
 
@@ -32,14 +33,51 @@ const App = () => {
     //  console.log(event.target.value);
   }
 
+  const onHandleDelete = (personId, name) => {
+    const confirmAlert = window.confirm(
+      `Do you really want to delete ${name}?`
+    )
+    if (confirmAlert === true) {
+      const newList = persons.filter(person => person.personId !== personId);
+      personService
+        .deleteId(personId)
+        .then(() => {
+          setPersons(newList);
+          console.log(personId);
+          window.location.reload(false);
+        })
+    }
+  }
+
+  const updateNumberOf = () => {
+    const person = persons.find(p => p.name === newName)
+    const confirmAlert = window.confirm(
+      `${newName} is already exist do you want to replace the old number?`
+    )
+    if (confirmAlert) {
+      const id = person.id;
+      const replacePerson = { ...person, number: newNumber }
+      console.log(person);
+      console.log(id);
+     return personService
+        .update(id, replacePerson)
+        .then(returnedPerson => {
+          setPersons(persons.map(person => person.id !== id ? person : returnedPerson))
+          console.log('success!');
+          setNewName('');
+          setNewNumber('');
+        })
+    }
+
+  }
+
   const addPerson = (event) => {
     event.preventDefault();
-
     const personObject = {
       name: newName,
       number: newNumber,
-      id: persons.length + 1
-    
+      id: ""
+
     }
     const str = `\\b${newName}\\b`;
     const wordMatch = str.trim().split(" ").join('');
@@ -49,11 +87,19 @@ const App = () => {
     //console.log(persons[1].name.split(" ").join(''));
 
     if (result.length > 0) {
-      alert(`${newName} is already exist!`)
+
+      updateNumberOf()
     } else {
-      setPersons(persons.concat(personObject));
-      setNewName('');
+      personService.
+        create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson));
+          setNewName('');
+          setNewNumber('');
+        })
     }
+
+
   }
   return (
     <div>
@@ -67,7 +113,7 @@ const App = () => {
         handleNumber={handleNumber}
       />
       <h2>Numbers</h2>
-      <Persons persons={persons} newFilter={newFilter} />
+      <Persons persons={persons} newFilter={newFilter} onDelete={onHandleDelete} updateNumberOf={updateNumberOf} />
     </div>
   )
 }
--- phonebook/src/components/Persons.js
+++ phonebook/src/components/Persons.js
@@ -1,20 +1,21 @@
 import React from "react";
 
-const Person = ({ name, number }) => {
+const Person = ({ name, number, onDelete }) => {
+
     return (
         <div>
-            <li>{name} {number}</li>
+            <li>{name} {number}<button onClick={onDelete}>delete</button></li>
         </div>
     )
 }
 
-const Persons = ({ persons, newFilter }) => {
+const Persons = ({ persons, newFilter, onDelete, updateNumberOf }) => {
     let filteredList = persons
     if (newFilter) {
         filteredList = persons.filter(el => new RegExp(newFilter.toLowerCase()).test(el.name.toLowerCase()))
     }
     return (
-        filteredList.map(person => <Person key={person.id} name={person.name} number={person.number} />)
+        filteredList.map(person => <Person key={person.id} name={person.name} number={person.number} onDelete={() => onDelete(person.id, person.name)}  />)
     )
 }
 
